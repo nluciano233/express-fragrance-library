02aa7cd41d5b8ad9e10d50753d90c3b5ee0fc6f4
nluciano233 <nluciano233@gmail.com>
2022-03-10 11:54:20 +0100

    feature: created the product model

---
+++ models/productModel.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+Schema = mongoose.Schema;
+
+var ProductSchema = Schema({
+  name: {
+    type: String,
+    min: [3, 'Name too short.'],
+    max: [70, 'Name too long.'],
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  },
+  price: Number,
+  stock: Number
+});
+
+// virtual for URL
+ProductSchema
+  .virtual('url')
+  .get(function() {
+    return '/product/' + this._id;
+  });
+
+// export model
+module.exports = mongoose.model('Product', ProductSchema)
