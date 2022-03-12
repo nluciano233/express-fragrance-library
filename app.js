d4c2333a0e95f9bde8005e0f79d5c7f3b160f247
nluciano233 <nluciano233@gmail.com>
2022-03-12 19:52:13 +0100

    feature: config env

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
-require('dotenv').config(); 
+require('dotenv').config();
+
 
 var indexRouter = require('./routes/index');
 var adminRouter = require('./routes/admin')
