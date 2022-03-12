b12f1c2000a80aabb1984a77891dbe3b99b669a2
nluciano233 <nluciano233@gmail.com>
2022-03-12 19:57:26 +0100

    feature: product create routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({ dest: 'public/images/uploads' })
 
-router.get('/inventory', function(req, res, next) {
-  res.render('admin/inventory', { title: 'Inventory' });
-});
+var admin_controller = require('../controllers/adminController');
+
+// router.get('/inventory', function(req, res, next) {
+//   res.render('admin/inventory', { title: 'Inventory' });
+// });
+
+router.get('/inventory', admin_controller.inventory);
+
+// get request for creating a product
+router.get('/product/create', admin_controller.product_create_get)
+
+// post request for creating a product
+router.post('/product/create', upload.single('product_image'), admin_controller.product_create_post);
 
 module.exports = router;
