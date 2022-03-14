f3fb36cef4a366867528919135ce59c98fcbf1eb
nluciano233 <nluciano233@gmail.com>
2022-03-14 13:06:38 +0100

    feature: product/create spam protection

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -73,70 +73,73 @@ const product_create_post = [
   // process request after validation and sanitization
   (req, res, next) => {
 
-    // extract the validation errors from request
-    const errors = validationResult(req);
-    
-    // create a product object with escaped and trimmed data
-    var product = new Product(
-      {
-        name: req.body.product_name,
-        description: req.body.product_description,
-        category: req.body.product_category,
-        image_id: req.file.filename,
-        price: (()=> {
-          var e = req.body.product_price
-          var priceArray = e.split("")
-          var arrLength = priceArray.length
-          var commaIndex = e.indexOf('.')
-          var decimalIndex = commaIndex + 2
-
-          if (!priceArray[decimalIndex]) {
-            // if the price does not have the decimal number
-            return e + '0'
-          }
-          if (!e.includes('.')) {
-            // if the price does not have cents
-            return e + '.00'
-          }
-
-          // if everything is all right return the price
-          return e
-        })(),
-        stock: req.body.product_stock
-      }
-      );
+    //execute code only if there is no anti_spam field in the req.body
+    if (!req.body.anti_spam) {
+      // extract the validation errors from request
+      const errors = validationResult(req);
       
-      if (!errors.isEmpty()) {
-        // there are errors. render form again with sanitized values/error messages
+      // create a product object with escaped and trimmed data
+      var product = new Product(
+        {
+          name: req.body.product_name,
+          description: req.body.product_description,
+          category: req.body.product_category,
+          image_id: req.file.filename,
+          price: (()=> {
+            var e = req.body.product_price
+            var priceArray = e.split("")
+            var arrLength = priceArray.length
+            var commaIndex = e.indexOf('.')
+            var decimalIndex = commaIndex + 2
 
-        Category //we find the categories for the dropdown selection
-        .find({}, 'name')
-        .exec(function (err, list_categories) {
-          if (err) {return next(err);}
+            if (!priceArray[decimalIndex]) {
+              // if the price does not have the decimal number
+              return e + '0'
+            }
+            if (!e.includes('.')) {
+              // if the price does not have cents
+              return e + '.00'
+            }
 
-          res.render('admin/product_form', 
-          {
-            title: 'Create product',
-            categories: list_categories,
-            product: product,
-            err: errors.array(),
+            // if everything is all right return the price
+            return e
+          })(),
+          stock: req.body.product_stock
+        }
+        );
+        
+        if (!errors.isEmpty()) {
+          // there are errors. render form again with sanitized values/error messages
+
+          Category //we find the categories for the dropdown selection
+          .find({}, 'name')
+          .exec(function (err, list_categories) {
+            if (err) {return next(err);}
+
+            res.render('admin/product_form', 
+            {
+              title: 'Create product',
+              categories: list_categories,
+              product: product,
+              err: errors.array(),
+            });
+
+            //delete the image that was saved in the host storage
+            fs.unlink('public/images/uploads/' + product.image_id, function(err) {
+              if (err) { return next(err); };
+            });
+
           });
-
-          //delete the image that was saved in the host storage
-          fs.unlink('public/images/uploads/' + product.image_id, function(err) {
-            if (err) { return next(err); };
+          return;
+        }
+        else {
+          // data from form is valid, save product
+          product.save(function(err) {
+            if (err) { return next(err); }
+            // successful, redirect to inventory
+            res.redirect('/admin/inventory')
           });
-
-        });
-        return;
-      }
-      else {
-        // data from form is valid, save product
-        product.save(function(err) {
-          if (err) { return next(err); }
-          // successful, redirect to inventory
-          res.redirect('/admin/inventory')
-        });
+        };
       };
     },
   ];
