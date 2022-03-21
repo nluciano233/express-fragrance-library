c9dd9309a0f08f4e1f622efe3d673606393c1847
nluciano233 <nluciano233@gmail.com>
2022-03-21 15:25:26 +0100

    fte: added express ejs layouts

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var expressLayouts = require('express-ejs-layouts');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -29,6 +30,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// ejs layouts middleware
+app.use(expressLayouts)
 
 app.use('/', indexRouter);
 app.use('/admin', adminRouter);
